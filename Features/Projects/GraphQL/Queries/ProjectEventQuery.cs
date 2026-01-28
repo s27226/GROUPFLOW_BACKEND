db1e3ebd1ecaa6136162ad6b63e47dec32b478b5
s27226 <s27226@pjwstk.edu.pl>
2026-01-28 21:53:11 +0100

    Fixed missing reference

--- Features/Projects/GraphQL/Queries/ProjectEventQuery.cs
+++ Features/Projects/GraphQL/Queries/ProjectEventQuery.cs
@@ -9,19 +9,24 @@ public class ProjectEventQuery
     [GraphQLName("allevents")]
     public async Task<List<ProjectEvent>> GetProjectEvents(AppDbContext context) 
     {
-        return await context.ProjectEvents.ToListAsync();
+        return await context.ProjectEvents
+            .Include(e => e.CreatedBy)
+            .ToListAsync();
     }
 
     [GraphQLName("eventbyid")]
     public async Task<ProjectEvent?> GetProjectEventById(AppDbContext context, int id) 
     {
-        return await context.ProjectEvents.FirstOrDefaultAsync(e => e.Id == id);
+        return await context.ProjectEvents
+            .Include(e => e.CreatedBy)
+            .FirstOrDefaultAsync(e => e.Id == id);
     }
 
     [GraphQLName("eventsbyproject")]
     public async Task<List<ProjectEvent>> GetEventsByProject(AppDbContext context, int projectId) 
     {
         return await context.ProjectEvents
+            .Include(e => e.CreatedBy)
             .Where(e => e.ProjectId == projectId)
             .ToListAsync();
     }
